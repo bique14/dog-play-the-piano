b7ed3931eeb0c044a07cfdacadaaf7e2cd1396bc
bique14 <peerasorn14@gmail.com>
2018-10-16 01:07:29 +0700

    can show lyrics in website

---
+++ lyrics.js
@@ -0,0 +1,66 @@
+function lyric_kfc() {
+  document.getElementById("lyric").innerHTML = `intro
+XxcBbBbn xcxcB
+BbBbn xcxcB
+bnbnmnbcB nBcx
+cBjx
+
+xxccBB hjjxxj 
+xxccBB hjjxxj
+jjXXxj XXxcxXx
+Bccxx
+
+xxccBB hjjxxj
+xxxccBB hjjxxj
+jjXXxj XXxxcxXx
+Bccxx
+
+mmmmmmnnBmn nnnnbBccx
+mmmmmmnnBmnn nnnnbBcxxjx
+Bccxxj Bccxxj
+cB cB cB cnBc
+Bncxxx
+
+cBcxBnBmn
+dBcxBBnBm - mnB
+jxc jxc
+BcBcBcBcBcBcBn
+BcBcBcBcBcBcBn
+BcBcBcBcBcBcBncxx`
+}
+
+function lyric_shonichi() {
+  document.getElementById("lyric").innerHTML = `xxxxzjzx xnbBbj ccBx
+xxxxzb jjjjhghhjj
+xxxxzjzx xnbBbjccBx
+xxxxzb zjhghhggg
+
+cccxc BBBcB xxxzxxzxcmnm
+cccxc BBBcB BBBcBBBcBmnmb
+nccccnncc BcccBbnm
+
+ความฝันต้องเกิดหยาดเหงื่อจึงได้มา ~`
+}
+
+function lyric_kimiwa() {
+  document.getElementById("lyric").innerHTML = `xxxxzjjz xxxczjh
+zzzzjhjzzxzjjhg
+jjhgghjjjzjhhgh hhghhghjhzj
+
+xxzjjzxx xcxzzjh
+zzjhhjzz zxjjjhg
+jjhgghjjjzjhhgh
+hgGgg
+
+ggjjxcccBcxjghj cccBcx
+jjhjjczjzxc ccxccBbB
+cBBbB
+
+ccBbBb bBb
+nnbBc BcB BcB
+jbBcxxxc cCcBjb gbBvbn
+ccB bBb bBb
+nnbBcBbnmb
+ccBbBcBb ccBbBbmmnbBnbb
+`
+}
