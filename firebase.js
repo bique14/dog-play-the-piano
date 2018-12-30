12d0ab5bfd267ed469bcf2beea7d48b388e18460
Peerasorn Hemsart <peerasorn@Peerasorns-MacBook-Pro.local>
2018-12-30 19:25:04 +0700

    through to the new year :)

--- firebase.js
+++ firebase.js
@@ -6,14 +6,27 @@ var config = {
   projectId: "dog-play-the-piano",
   storageBucket: "",
   messagingSenderId: "78968550181"
-};
-firebase.initializeApp(config);
+}
 
-var database = firebase.database();
+firebase.initializeApp(config)
+var database = firebase.database()
 
-//create a variable to hold our orders list from firebase
-var lyricList = database.ref().child('lyrics');
+var buttons = document.getElementsByTagName('button')
 
-lyricList.on('value', snapshot => {
-  console.log(snapshot.val())
-})
+$('button').click(function () {
+  const id = this.id
+  console.log(id)
+
+  var lyricList = database.ref().child('lyrics')
+  lyricList.on('value', snapshot => {
+    console.log(snapshot.val())
+    if (id === 'btn-kuenkampee') {
+      const da = lyricList.child(`da-endrophine/คืนข้ามปี`)
+      da.on('value', snapshot => {
+        const result = snapshot.val().replace(/\s+/g, '\n')
+        $("#lyric").html(result)
+      })
+    }
+  })
+});
+
