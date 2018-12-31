6b9140fe3a803a7d580bd389be4832ae95111f03
Peerasorn Hemsart <peerasorn@Peerasorns-MacBook-Pro.local>
2018-12-31 21:07:19 +0700

    Christmas eve

--- firebase.js
+++ firebase.js
@@ -22,9 +22,15 @@ $('button').click(function () {
     console.log(snapshot.val())
     if (id === 'btn-kuenkampee') {
       const da = lyricList.child(`da-endrophine/คืนข้ามปี`)
-      da.on('value', snapshot => {
-        const result = snapshot.val().replace(/\s+/g, '\n')
+      da.on('value', lyric => {
+        const result = lyric.val().replace(/\s+/g, '\n')
         $("#lyric").html(result)
+      })
+    } else if (id === 'btn-anatato') {
+      const anataToChristEve = lyricList.child('bnk48/anata to christmas eve')
+      anataToChristEve.once('value', lyric => {
+        const result = lyric.val().replace(/\s{1}/g, '\n').replace(/\s{2}/g, '\n\n')
+        $("#lyric").html(result)        
       })
     }
   })
