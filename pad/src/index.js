f1b2b22ca6d32bb70e3287aabb188c4faa7f0ac2
bbibbibiq <peerasorn14@gmail.com>
2020-06-21 20:36:02 +0700

    Fix style, using loop get element

--- pad/src/index.js
+++ pad/src/index.js
@@ -1,110 +1,35 @@
 var socket = io.connect();
 const containerKey = document.getElementById("keys-flex");
 
-const keys = [
-  "c-",
-  "db-",
-  "d-",
-  "eb-",
-  "e-",
-  "f-",
-  "gb-",
-  "g-",
-  "ab-",
-  "a-",
-  "bb-",
-  "b-",
-  "c",
-  "db",
-  "d",
-  "eb",
-  "e",
-  "f",
-  "gb",
-  "g",
-  "ab",
-  "a",
-  "bb",
-  "b",
-  "c+",
-  "db+",
-  "d+",
-  "eb+",
-  "e+",
-  "f+",
-  "gb+",
-  "g+",
-  "ab+",
-  "a+",
-  "bb+",
-  "b+",
-];
+const keys = [ "c-", "db-", "d-", "eb-", "e-", "f-", "gb-", "g-", "ab-", "a-", "bb-", "b-", "c", "db", "d", "eb", "e", "f", "gb", "g", "ab", "a", "bb", "b", "c+", "db+", "d+", "eb+", "e+", "f+", "gb+", "g+", "ab+", "a+", "bb+", "b+" ];
 
 const flatKeys = /[a-z]{1}[b]([+|-])?/g;
-const afterBlackKeys = /[b|e]{1}([+|-])?/g
+const afterBlackKeys = /[b|e]{1}([+|-])?/g;
 keys.map((k, i) => {
   let buttonKey = document.createElement("li");
   buttonKey.id = k;
   buttonKey.innerHTML = k;
 
-  if(k.match(flatKeys)) {
-    buttonKey.className = "black-key"
+  if (k.match(flatKeys)) {
+    buttonKey.className = "black-key";
   } else {
-    buttonKey.className = "white-key"
-    if(k.match(afterBlackKeys)) {
-      buttonKey.style.width = "5rem"
+    buttonKey.className = "white-key";
+    if (k.match(afterBlackKeys)) {
+      buttonKey.style.width = "5rem";
     }
   }
 
   containerKey.appendChild(buttonKey);
 });
 
-const C = document.getElementById("C");
-const D = document.getElementById("D");
-const E = document.getElementById("E");
-const F = document.getElementById("F");
-const G = document.getElementById("G");
-const A = document.getElementById("A");
-const B = document.getElementById("B");
+const keyElementName = [ "Clow", "DlowB", "Dlow", "ElowB", "Elow", "Flow", "GlowB", "Glow", "AlowB", "Alow", "BlowB", "Blow", "Cmiddle", "DmiddleB", "Dmiddle", "EmiddleB", "Emiddle", "Fmiddle", "GmiddleB", "Gmiddle", "AmiddleB", "Amiddle", "BmiddleB", "Bmiddle", "Chigh", "DhighB", "Dhigh", "EhighB", "Ehigh", "Fhigh", "GhighB", "Ghigh", "AhighB", "Ahigh", "BhighB", "Bhigh" ];
+keyElementName.map(function(e, i) {
+  window[`${e}`] = document.getElementById(`${keys[i]}`)
 
-C.addEventListener("click", () => {
-  socket.emit("type", {
-    type: "C-key",
-  });
-});
-
-D.addEventListener("click", () => {
-  socket.emit("type", {
-    type: "D-key",
-  });
-});
-
-E.addEventListener("click", () => {
-  socket.emit("type", {
-    type: "E-key",
-  });
-});
-
-F.addEventListener("click", () => {
-  socket.emit("type", {
-    type: "F-key",
-  });
-});
-
-G.addEventListener("click", () => {
-  socket.emit("type", {
-    type: "G-key",
-  });
-});
-
-A.addEventListener("click", () => {
-  socket.emit("type", {
-    type: "A-key",
-  });
-});
-
-B.addEventListener("click", () => {
-  socket.emit("type", {
-    type: "B-key",
+  window[`${e}`].addEventListener("click", () => {
+    console.log('click!', e)
+    socket.emit("type", {
+      type: e,
+    });
   });
 });
