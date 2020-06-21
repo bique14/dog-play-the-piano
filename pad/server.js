064f0fda378f6f03566475c83d8a7ca0fb1393c0
bbibbibiq <peerasorn14@gmail.com>
2020-06-21 20:36:25 +0700

    Tap with mobile! 📱

--- pad/server.js
+++ pad/server.js
@@ -1,42 +1,72 @@
-var app = require('express')()
-var server = require('http').Server(app)
-var io = require('socket.io')(server)
+var app = require("express")();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
-const robotjs = require('robotjs')
-const port = 8080
+const robotjs = require("robotjs");
+const port = 8080;
 
 server.listen(port, (err) => {
-  if (err) throw err
-  console.log('listening on', server.address())
-})
+  if (err) throw err;
+  console.log("listening on", server.address());
+});
 
-app.use(require('express').static('src'))
+app.use(require("express").static("src"));
 
-io.on('connection', function (socket) {
-  socket.on('type', client => {
-    const { type } = client
-    switch (type) {
-      case 'C-key':
-        robotjs.keyTap('a')
-        break;
-      case 'D-key':
-        robotjs.keyTap('s')
-        break;
-      case 'E-key':
-        robotjs.keyTap('d')
-        break;
-      case 'F-key':
-        robotjs.keyTap('f')
-        break;
-      case 'G-key':
-        robotjs.keyTap('g')
-        break;
-      case 'A-key':
-        robotjs.keyTap('h')
-        break;
-      case 'B-key':
-        robotjs.keyTap('j')
-        break;
-    }
-  })
-})
+const keyObject = {
+  "Clow": "q",
+  "DlowB": "W",
+  "Dlow": "w",
+  "ElowB": "E",
+  "Elow": "e",
+  "Flow": "r",
+  "GlowB": "T",
+  "Glow": "t",
+  "AlowB": "Y",
+  "Alow": "y",
+  "BlowB": "U",
+  "Blow": "u",
+
+  "Cmiddle": "a",
+  "DmiddleB": "S",
+  "Dmiddle": "s",
+  "EmiddleB": "D",
+  "Emiddle": "d",
+  "Fmiddle": "f",
+  "GmiddleB": "G",
+  "Gmiddle": "g",
+  "AmiddleB": "H",
+  "Amiddle": "h",
+  "BmiddleB": "J",
+  "Bmiddle": "j",
+
+  "Chigh": "z",
+  "DhighB": "X",
+  "Dhigh": "x",
+  "EhighB": "C",
+  "Ehigh": "c",
+  "Fhigh": "v",
+  "GhighB": "B",
+  "Ghigh": "b",
+  "AhighB": "N",
+  "Ahigh": "n",
+  "BhighB": "M",
+  "Bhigh": "m"
+};
+
+
+const fleyKeyRegex = /[\w]+[B]/g;
+io.on("connection", function (socket) {
+  socket.on("type", (client) => {
+    const { type } = client;
+
+    Object.entries(keyObject).forEach(([key, value]) => {
+      if(type == key) {
+        if (key.match(fleyKeyRegex)) {
+          robotjs.keyTap(value.toLowerCase(), ['shift'])
+        } else {
+          robotjs.keyTap(value)
+        }
+      }
+    })
+  });
+});
